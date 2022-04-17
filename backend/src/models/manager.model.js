8e4b1d9f3916ee5e31aa5ea655a2f7cfe9582050
Anjum Ara <anjumara0711@gmail.com>
2022-04-18 02:46:48 +0530

    some changes in manager model

--- backend/src/models/manager.model.js
+++ backend/src/models/manager.model.js
@@ -1,16 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+const bcrypt = require("bcryptjs");
 
 const managerSchema = new mongoose.Schema({
-    name:{type: 'string',required: true},
-    age:{type: 'number',required: true},
-    gender:{type: 'string',required: true},
-    apartmentId : {type: mongoose.Schema.Types.ObjectId, ref: "apartment", required: true}
-    
-},{
-    versionKey:false,
-    timestamps:true,
+    name: {type:String,required:true},
+    email: {type:String,required:true},
+    password: {type:String,required:true},
+    phone: {type:Number,required:true},
+    gender:{type:String,required:true},
+
+ },{
+     versionKey:false,
+     timestamps:true
+ })
+
+//  hash the password before saving
+// managerSchema.pre('save', function(next){
+ managerSchema.pre("save", function(next){
+
+    // if password is not modified then return
+    if(!this.isModified('password')) return next();
+  
+    const hash = bcrypt.hashSync(this.password,6);
+    this.password = hash;
+    return next();
+ })
+
+// function comparePassword(password){
+// to compare the password while login
+managerSchema.methods.comparePassword = function(password){
+    // if same then return true
+    // else return false
+    return bcrypt.compareSync(password,this.password);
 }
 
-)
+const Manager = mongoose.model("Manager",managerSchema);
 
-module.exports = mongoose.model("manager",managerSchema);
+module.exports = Manager; 
